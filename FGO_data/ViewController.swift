428774d826c9f69bcdc1f8de6cb3301b53504766
AutumnCAT <autumncat@lab414de-imac.local>
2018-04-10 20:45:04 +0800

    FGO_data 1.0

--- FGO_data/ViewController.swift
+++ FGO_data/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Thread.sleep(forTimeInterval: 2.0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+       
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
 }
 
